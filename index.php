b57f4b158331067d1ca44a67b125562a27a41035
Jholin Ibrahimovic <jholin04@gmail.com>
2022-12-29 11:37:59 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum culpa laborum quod tempora esse quis asperiores recusandae incidunt voluptates? Praesentium veritatis, molestias quisquam deleniti omnis officiis laudantium exercitationem provident. Voluptatum.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
